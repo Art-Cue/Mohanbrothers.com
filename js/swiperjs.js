d810875cb2122f368bcf1bbf8afbadf810467e2b
Art-Cue <artcue.design@gmail.com>
2026-06-27 17:39:37 +0530

    Complete responsive navbar and hero section with Swiper.js

---
+++ js/swiperjs.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+var swiper = new Swiper('.hero-main-swiper', {
+loop: true,
+observer: true,
+    observeParents: true,
+    updateOnWindowResize: true,
+
+pagination: {
+el: '.swiper-pagination',
+clickable: true,
+},
+
+navigation: {
+nextEl: '.swiper-button-next',
+prevEl: '.swiper-button-prev',
+},
+
+// autoplay: {
+// delay: 1700,
+// pauseOnMouseEnter: true,
+// },
+});
+
+});
